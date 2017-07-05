5caee84b81c151f23c8aba5aa80be166492b508d
caccon <thekien95@@gmail.com>
2017-07-05 15:15:51 +0700

    test

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ function processPostback(event) {
       var message = greeting + "VAPE Tau Dien Tu xin chao";
       var message1 = "Shop là nơi uy tín hàng đầu dành cho vaper tại Việt nam với nguyên tắc.Nói không với clone!";
       var message2 = "Bắt đầu chat với chúng tôi hoặc lựa chọn các menu tư vấn nhanh dưới đây";
-      sendMessage(senderId, {text: message, message1, message2});
+      sendMessage(senderId, {text: message});
+      sendMessage(senderId, {text: message1});
+      sendMessage(senderId, {text: message2});
     });
   }
 }
