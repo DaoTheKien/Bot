e20d8aac01534c57514ab5af133880f51492a699
caccon <thekien95@@gmail.com>
2017-07-14 16:48:17 +0700

    test

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function processPostback(event) {
       var message = greeting + "VAPE Tau Dien Tu xin chao";
       var message1 = "Shop là nơi uy tín hàng đầu dành cho vaper tại Việt nam với nguyên tắc.Nói không với clone!";
       var message2 = "Bắt đầu chat với chúng tôi hoặc lựa chọn các menu tư vấn nhanh dưới đây";
-      var message3 = "Đây là một số VAPE & tinh dầu mới về tại shop. Xin mời bạn xem qua nhé";
+
       var first = {
         "type":"template",
         "payload":{
@@ -104,6 +104,55 @@ function processPostback(event) {
           ]
         }
       };
+
+      setTimeout( function () {
+        sendMessage(senderId, {text: message});
+      }, 0);
+
+      setTimeout( function () {
+        sendMessage(senderId, {text: message1});
+      },1000);
+
+      setTimeout( function () {
+        sendMessage(senderId, {text: message2});
+      },2000);
+
+      setTimeout( function () {
+        sendMessage(senderId, {attachment: first});
+      }, 3000);
+
+
+
+    });
+  };
+
+  // if (payload === "SHOPPING_PAYLOAD") {
+  //
+  //   request({
+  //     url: url,
+  //     json: true
+  //   }, (err, response, body) => {
+  //     var customCollection = body.custom_collection;
+  //     customCollection[0].image = {'src': 'https://external.xx.fbcdn.net/safe_image.php?d=AQBbJwEqF8YUvzsK&url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1023%2F1927%2Fproducts%2FMUTXuXjfQxymW1HG8ZJq_Original-samsung-icr18650-icr-18650-icr18650-22f-Protected-18650-battery-high-quality-2200mah-3-7v-li_764x400_pad.jpg%3Fv%3D1492238290&_nc_hash=AQAyPcWAQfxSKO53'}
+  //     customCollection[1].image = {'src': 'https://external.xx.fbcdn.net/safe_image.php?d=AQCdHhci4GwBImIl&url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1023%2F1927%2Fproducts%2FBrewichDare_764x400_pad.jpg%3Fv%3D1492237919&_nc_hash=AQCnJxSrx5ajYx8K'}
+  //     customCollection[2].image = {'src': 'https://external.xx.fbcdn.net/safe_image.php?d=AQDKJ-ajdU1mHB76&url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1023%2F1927%2Fproducts%2Fewsad_764x400_pad.jpg%3Fv%3D1492237924&_nc_hash=AQAaF61ORoCjlDZ4'}
+  //     sendMessage(senderId, {text: messages3});
+  //     shopify.smartCollection.list()
+  //     .then(collection => {
+  //       collection = customCollection.concat(collection);
+  //       sendMessage(senderId, display(collection));
+  //     })
+  //     .catch(err => console.error(err));
+  //   });
+  // };
+
+  if (payload === "SHOPPING_PAYLOAD") {
+
+    request({
+      url:url,
+      json: true
+    }, (err, response, body) => {
+      var message3 = "Đây là một số VAPE & tinh dầu mới về tại shop. Xin mời bạn xem qua nhé";
       var productTest = {
         "type":"template",
         "payload":{
@@ -160,121 +209,19 @@ function processPostback(event) {
         }
 
       }
-      setTimeout( function () {
-        sendMessage(senderId, {text: message});
-      }, 0);
 
-      setTimeout( function () {
-        sendMessage(senderId, {text: message1});
-      },1000);
 
-      setTimeout( function () {
-        sendMessage(senderId, {text: message2});
-      },2000);
-
-      setTimeout( function () {
-        sendMessage(senderId, {attachment: first});
-      }, 5000);
       setTimeout( function () {
         sendMessage(senderId, {text:message3});
-      }, 3000);
+      }, 1000);
 
       setTimeout( function () {
         sendMessage(senderId, {attachment: productTest});
-      }, 4000)
-
-
+      }, 2000)
     });
+
+
   };
-
-  // if (payload === "SHOPPING_PAYLOAD") {
-  //
-  //   request({
-  //     url: url,
-  //     json: true
-  //   }, (err, response, body) => {
-  //     var customCollection = body.custom_collection;
-  //     customCollection[0].image = {'src': 'https://external.xx.fbcdn.net/safe_image.php?d=AQBbJwEqF8YUvzsK&url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1023%2F1927%2Fproducts%2FMUTXuXjfQxymW1HG8ZJq_Original-samsung-icr18650-icr-18650-icr18650-22f-Protected-18650-battery-high-quality-2200mah-3-7v-li_764x400_pad.jpg%3Fv%3D1492238290&_nc_hash=AQAyPcWAQfxSKO53'}
-  //     customCollection[1].image = {'src': 'https://external.xx.fbcdn.net/safe_image.php?d=AQCdHhci4GwBImIl&url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1023%2F1927%2Fproducts%2FBrewichDare_764x400_pad.jpg%3Fv%3D1492237919&_nc_hash=AQCnJxSrx5ajYx8K'}
-  //     customCollection[2].image = {'src': 'https://external.xx.fbcdn.net/safe_image.php?d=AQDKJ-ajdU1mHB76&url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F1023%2F1927%2Fproducts%2Fewsad_764x400_pad.jpg%3Fv%3D1492237924&_nc_hash=AQAaF61ORoCjlDZ4'}
-  //     sendMessage(senderId, {text: messages3});
-  //     shopify.smartCollection.list()
-  //     .then(collection => {
-  //       collection = customCollection.concat(collection);
-  //       sendMessage(senderId, display(collection));
-  //     })
-  //     .catch(err => console.error(err));
-  //   });
-  // };
-
-  if (payload === "SHOPPING_PAYLOAD") {
-
-    request({
-      url:url,
-      json: true
-    }, (err, response, body) => {
-      var collection = {
-        "type":"template",
-        "payload":{
-          "template_type":"generic",
-          "image_aspect_ratio":"square",
-          "elements":[
-            {
-              "title":"Khuyến mãi giới thiệu tinh dầu vape VADER đến 31/05/2017",
-              "image_url":"http://imagizer.imageshack.us/480x480f/923/SLsn7S.jpg",
-              "subtitle":"Cơ hội đặc biệt mua tinh dầu vape với giá siêu khuyến mãi chỉ 220,000đ",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://www.taudientu.com/collections/khuy-n-mai-tinh-d-u-vape-20-03-09-04-2017"
-              },
-              "buttons":[
-                {
-                  "type":"web_url",
-                  "url":"https://www.taudientu.com/collections/khuy-n-mai-tinh-d-u-vape-20-03-09-04-2017",
-                  "title":"Chi tiết",
-                }
-              ]
-            }, {
-              "title":"Tổng hợp Top 100 tinh dầu Vape 2017 tại #Taudientu",
-              "image_url":"http://imagizer.imageshack.us/480x480f/923/t9qKwC.jpg",
-              "subtitle":"Tổng hợp list 100 tinh dầu vape bán chạy nhất, được ưa chuộng nhất tại #Tẩu Điện Tử",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://www.taudientu.com/collections/t-ng-h-p-top-100-tinh-d-u-vape-2017"
-              },
-              "buttons":[
-                {
-                  "type":"web_url",
-                  "url":"https://www.taudientu.com/collections/t-ng-h-p-top-100-tinh-d-u-vape-2017",
-                  "title":"Chi tiết",
-                }
-              ]
-            }, {
-              "title":"Châm tinh dầu là xài được ngay! Trọn bộ VAPE dành cho người mới",
-              "image_url":"http://imagizer.imageshack.us/480x480f/924/NWEB5J.jpg",
-              "subtitle":"Combo vape giá cực ưu đãi dành cho người mới bắt đầu",
-              "default_action": {
-                "type": "web_url",
-                "url": "https://www.taudientu.com/collections/t-u-di-n-t"
-              },
-              "buttons":[
-                {
-                  "type":"web_url",
-                  "url":"https://www.taudientu.com/collections/t-u-di-n-t",
-                  "title":"Chi tiết",
-                }
-              ]
-            }
-          ]
-        }
-      };
-
-    sendMessage(senderId, {attachment: collection});
-    dvnldklvnkv
-    })
-
-
-  }
 };
 
 // function processMessage(event) {
